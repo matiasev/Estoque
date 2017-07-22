d664319b0911b694c98a3c81a905299b3c39dc64
Evandro Matias <evandromattias@gmail.com>
2017-07-22 00:17:56 -0300

    add data

---
+++ EstoqueData/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿//using EstoqueData.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace EstoqueData.Data
+//{
+//    public static class DbInitializer
+//    {
+//        public static void Initialize(EstoqueContext context)
+//        {
+//            //context.Database.EnsureCreated();
+
+//            if (context.Produtos.Any())
+//            {
+//                return;
+//            }
+
+//            var Produtos = new Produto[]
+//            {
+//                new Produto{ Nome="Geladeira", Quantidade=12, Preco=22 },
+//                new Produto{ Nome="Cama", Quantidade=3, Preco=1300},
+//                new Produto{ Nome="Maquina de lavar", Quantidade=5, Preco=2000}
+//            };
+
+//            foreach (Produto i in Produtos)
+//            {
+//                context.Produtos.Add(i);
+//            }
+
+//            context.SaveChanges();
+//        }
+//    }
+//}
---
+++ EstoqueData/Data/EstoqueContext.cs
@@ -0,0 +1,24 @@
+﻿using EstoqueData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstoqueData.Data
+{
+    public class EstoqueContext: DbContext
+    {
+        public EstoqueContext(DbContextOptions<EstoqueContext> options)
+            :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
