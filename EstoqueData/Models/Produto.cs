6db67552bb9aa443263898de61b02cbe9964dabe
Evandro Matias <evandromattias@gmail.com>
2017-07-22 00:20:28 -0300

    add model

---
+++ EstoqueData/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstoqueData.Models
+{
+    public class Produto
+    {
+        public int ProdutoID { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public int Preco { get; set; }
+    }
+}
