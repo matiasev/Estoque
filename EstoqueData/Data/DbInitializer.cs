eee1a8873c33536e45dfe56f7477e98352bad47d
Evandro Matias <evandromattias@gmail.com>
2017-08-07 17:35:14 -0300

    refatoração

--- EstoqueData/Data/DbInitializer.cs
+++ EstoqueData/Data/DbInitializer.cs
@@ -19,9 +19,9 @@ namespace EstoqueData.Data
 
             var Produtos = new Produto[]
             {
-                new Produto{ Nome="Geladeira", Quantidade=12, Preco=22, FornecedorID=1 },
-                new Produto{ Nome="Cama", Quantidade=3, Preco=1300, FornecedorID=1 },
-                new Produto{ Nome="Maquina de lavar", Quantidade=5, Preco=2000, FornecedorID=1 }
+                new Produto{ Nome="Geladeira", Quantidade=12, Preco=22, DataEntrada=DateTime.Parse("05-08-2017"), FornecedorID=1 },
+                new Produto{ Nome="Cama", Quantidade=3, Preco=1300, DataEntrada=DateTime.Parse("06-08-2017"), FornecedorID=1 },
+                new Produto{ Nome="Maquina de lavar", Quantidade=5, DataEntrada=DateTime.Parse("07-08-2017"), Preco=2000, FornecedorID=1 }
             };
 
             foreach (Produto p in Produtos)
