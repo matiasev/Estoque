5b14f7f30726c4537623742112bd939516712b4e
Evandro Matias <evandromattias@gmail.com>
2017-08-07 16:22:08 -0300

    add Test

---
+++ EstoqueTest/Add.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace EstoqueTest
+{
+    public class Add: Base
+    {
+        [Test]
+        public void ExecuteTest()
+        {
+            driver.FindElementByName("nome").SendKeys("Bala");
+
+            driver.FindElementByName("quantidade").SendKeys("123");
+
+            driver.FindElementByName("preco").SendKeys("32");
+
+            driver.FindElementByName("fornecedor").SendKeys("Luiz");
+           
+            driver.FindElementByClassName("btn-primary").Click();
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            wait.Until(x => x.SwitchTo().Alert());
+
+            driver.SwitchTo().Alert().Accept();
+
+            Console.WriteLine("Executed Test");
+        }
+
+    }
+}
---
+++ EstoqueTest/Base.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace EstoqueTest
+{
+    public class Base
+    {
+        protected ChromeDriver driver;
+
+        public Base()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("http://localhost:51154/");
+
+            Console.WriteLine("Opened URL");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            Thread.Sleep(1000);
+            driver.Close();
+
+            Console.WriteLine("Closed");
+        }
+    }
+}
